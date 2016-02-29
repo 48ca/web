0c7e8f56e257c1b5caf89b363e54dfeb6d3deabb
jamesthoughton <jamesthoughton@gmail.com>
2016-02-29 10:12:40 -0500

    Add iframes to index

--- labs/index.php
+++ labs/index.php
@@ -1,15 +1,32 @@
 <html>
 	<head>
+		<style>
+		.wrap { width: 600px; height: 390px; padding: 0; overflow: hidden; display:inline-block; float:left; }
+		iframe { width: 800px; height: 520px; border: 1px solid black; }
+		iframe {
+			-ms-zoom: 0.75;
+			-moz-transform: scale(0.75);
+			-moz-transform-origin: 0 0;
+			-o-transform: scale(0.75);
+			-o-transform-origin: 0 0;
+			-webkit-transform: scale(0.75);
+			-webkit-transform-origin: 0 0;
+		}
+		.list {
+			position:relative;
+		}
+		</style>
 	</head>
 	<body>
-		<ol>
+		<div class="list">
 		<?php
 			$dir = scandir('.');
 			foreach($dir as $w) {
 				if($w[0] != '0') continue;
-				echo '<li><a href="'.$w.'">Lab '.$w.'</a></li>';
+				echo '<a href="'.$w.'">Lab '.$w.'</a>';
+				echo '<div class="wrap"><iframe src="'.$w.'"></iframe></div>';
 			}
 		?>
-		</ol>
+		</div>
 	</body>
 </html>
