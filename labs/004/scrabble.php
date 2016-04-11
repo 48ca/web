80afd0207996b3dffc38a92809b1fd1f9bda878e
jamesthoughton <jamesthoughton@gmail.com>
2016-04-11 09:23:44 -0400

    Highlight matched letters

--- labs/004/scrabble.php
+++ labs/004/scrabble.php
@@ -1,21 +1,32 @@
 <?php
-    function find($r,$len) {
+    function highlight($r, $x) {
+        $ret = "";
+        foreach(str_split($r) as $l) {
+            if(strpos($x,$l) !== false) {
+                $ret.="<span class='found'>".$l."</span>";
+            } else {
+                $ret.=$l;
+            }
+        }
+        return $ret;
+    }
+    function find($r,$len,$extra) {
         // $words = file_get_contents("/usr/share/dict/words");
         $words = file_get_contents("dict.txt");
         if(!$words) return "";
         // echo($r);
-        preg_match_all("/\b(".$r.")\b/", $words, $keys);
+        preg_match_all("/\b".$r."\b/", $words, $keys);
         $ret = "";
         $fwords = $keys[0];
         if($len > 0) {
             foreach($fwords as $w) {
                 if(strlen($w) == $len) {
-                    $ret .= $w."<br>";
+                    $ret .= highlight($w,$extra)."<br>";
                 }
             }
         } else {
             foreach($fwords as $w) {
-                $ret .= $w."<br>";
+                $ret .= highlight($w,$extra)."<br>";
             }
         }
         if(strlen($ret) > 0) {
@@ -36,10 +47,10 @@
     $len = 0+$_GET["len"];
     switch($_GET["f"]) {
         case "start":
-            find($l.'.*',$len);
+            find($l.'.*',$len,$l);
             break;
         case "end":
-            find('.*'.$l,$len);
+            find('.*'.$l,$len,$l);
             break;
         case "contain":
             $regex = "";
@@ -52,7 +63,7 @@
                 $regex.="(?=".str_repeat("\w*".$let,$num).")";
             }
             $regex.="\w{".strlen($l).",}";
-            find($regex,$len);
+            find($regex,$len,$l);
             break;
     }
     
